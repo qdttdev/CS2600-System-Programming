1eff143c9d202fcb03a3d65123e6bed77cb10c49
Caroline Ta <qdttdev@gmail.com>
2021-02-09 16:23:35 -0800

    Update HW1

--- HW_1/HW_1.c
+++ HW_1/HW_1.c
@@ -18,31 +18,27 @@ int main(void)
 	int userInput = 0;
 	int pcInput = 0;
 	bool validInput;
-	bool draw = true;
 
 	// Clear the buffer
 	setbuf(stdout, NULL);
-
 	// Set seed to generate different a number every time the program runs
 	srand(time(0));
-
 	// 1. Generate a random number from 10-100
 	count = rand() % 91 + 10;
 
+	// 2. Inform the player of the number of marbles, count
+	printf("MARBLES COUNT: %d\n", count);
 
 	// 8. Go back to step 2, unless either the program or the user wins
 	while(count > 1)
 	{
-		// 2. Inform the player of the number of marbles, count
-			printf("MARBLES COUNT: %d\n", count);
-
 		// 3. Determine the largest value the user can take: 1/2 of count
 		max = count/2;
 
 		// 4. Prompt the user to enter a value between 1 and the max value in #3
 		do
 		{
-			printf("User     input (1-%d): ", max);
+			printf("User input (1-%d): ", max);
 			scanf("%d", &userInput);
 			validInput = userInput >= 1 && userInput <= max;
 
@@ -56,11 +52,12 @@ int main(void)
 
 		// 6. Subtract the value from count
 		count -= userInput;
+		max = count/2;
+		printf("\nMARBLES COUNT: %d\n", count);
+
 		if(count == 1)
 		{
-			printf("\nMARBLES COUNT: %d\n", count);
 			printf("\n***** Computer takes the last marble - User Wins! *****\n");
-			draw = false;
 			break;
 		}
 
@@ -68,24 +65,13 @@ int main(void)
 		pcInput = rand() % max + 1;
 		printf("Computer input (1-%d): %d\n\n", max, pcInput);
 		count -= pcInput;
+		printf("\nMARBLES COUNT: %d\n", count);
 
 		if(count == 1)
 		{
-			printf("\nMARBLES COUNT: %d\n", count);
 			printf("\n***** User takes the last marble - Computer Wins! *****\n");
-			draw = false;
 		}
 	}
 
-	if(draw)
-	{
-		printf("***** It's a tie! - Both User and Computer wins! *****");
-	}
-
-
 	return 0;
 }
-
-
-
-
